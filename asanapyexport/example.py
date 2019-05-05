2f0a483775728300c3765790995ee1a19e86adbc
amelie <amelie.rolland.1@gmail.com>
2019-05-05 16:04:32 -0400

    add subtasks in example

--- asanapyexport/example.py
+++ asanapyexport/example.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     logging.getLogger().setLevel(logging.INFO)
     auth_token = "replace by your token here"
     save_file_path = "save/path/my-asana-data.json"
-    export_projects_and_tasks_to_json(auth_token, save_file_path)
+    export_projects_and_tasks_to_json(auth_token, save_file_path, is_including_subtasks=True)
